5accb41363bc03cb0e31dd8363e4406105069db3
mhans <mhans@tu-berlin.de>
2025-02-07 20:37:48 +0100

    Graph Zeug auf Dev?!.

--- src/app/(default)/politician/[uuid]/page.tsx
+++ src/app/(default)/politician/[uuid]/page.tsx
@@ -9,6 +9,7 @@ import {
   PoliticianDetails,
 } from '@/app/(default)/politician/[uuid]/PoliticianDetails';
 import CommentSectionWrapper from '@/app/(default)/politician/[uuid]/comments/CommentSectionWrapper';
+import PoliticianGraph from './PoliticianGraph';
 
 async function fetchPolitician(uuid: string) {
   return db.politician.findFirst({
@@ -27,26 +28,34 @@ export default async function PoliticianView({
   params: Promise<{ uuid: string }>;
 }) {
   const polId = (await params).uuid;
-  const politician = fetchPolitician(polId);
+  const politician = await fetchPolitician(polId);
+
+  const politicianNumericId = politician?.ext_abgeordnetenwatch_id;
+  if (!politicianNumericId) {
+    return <div>Politician not found or missing rating info.</div>;
+  }
 
   return (
-    <section className={'pt-24'}>
+    <section className="pt-24">
       <div className="mx-auto max-w-6xl">
         <React.Suspense fallback={<LoadingPoliticianDetails />}>
-          <PoliticianDetails politicianPromise={politician} />
+          <PoliticianDetails politicianPromise={Promise.resolve(politician)} />
         </React.Suspense>
 
-        <h1 className={'mt-8 text-2xl font-semibold dark:text-gray-200'}>
-          Nebentätigkeiten:
-        </h1>
+        <h1 className="mt-8 text-2xl font-semibold">Nebentätigkeiten:</h1>
         <React.Suspense fallback={<LoadingSideJobs />}>
-          <SideJobs politicianPromise={politician} />
+          <SideJobs politicianPromise={Promise.resolve(politician)} />
         </React.Suspense>
 
-        <h1 className={'mt-8 text-2xl font-semibold dark:text-gray-200'}>
-          Kommentare:
-        </h1>
-        <CommentSectionWrapper politicianPromise={politician} />
+        <h1 className="mt-8 text-2xl font-semibold">Kommentare:</h1>
+        <CommentSectionWrapper
+          politicianPromise={Promise.resolve(politician)}
+        />
+
+        {/*Graph inserted here*/}
+        <React.Suspense fallback={<div>Loading Graph...</div>}>
+          <PoliticianGraph politicianId={politicianNumericId} />
+        </React.Suspense>
       </div>
     </section>
   );
