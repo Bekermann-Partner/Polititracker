0431cde3d21ad7ab01c513d18150114531ab5bca
Marcel Mende <marcel16.mende@gmail.com>
2025-02-10 21:45:34 +0100

    removed password requirement for profile edits that do not inlcude a new password

--- src/app/(default)/dashboard/edit/ProfileEditor.tsx
+++ src/app/(default)/dashboard/edit/ProfileEditor.tsx
@@ -227,7 +227,6 @@ export default function ProfileEditor({ user }: { user: User }) {
                 value={oldPassword}
                 onChange={(e) => setOldPassword(e.target.value)}
                 autoComplete="current-password"
-                required
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border  placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-800 dark:border-gray-600 outline-none dark:text-white"
               />
             </div>
